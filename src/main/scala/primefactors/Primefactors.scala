d12095f7927648e368c5b4550134988a56197bc4
gaur4vgaur <vaibgaurav@gmail.com>
2018-12-10 06:58:11 +0000

    prime factors solution

--- src/main/scala/primefactors/Primefactors.scala
+++ src/main/scala/primefactors/Primefactors.scala
@@ -1,5 +1,21 @@
 package primefactors
 
+import scala.annotation.tailrec
+
 object PrimeFactors {
-  def primeFactors(n: Int): Seq[Int] = Seq(n)
+  def primeFactors(n: Int): Seq[Int] = {
+
+    @tailrec
+    def innerPrimeFactor(n: Int, factors: Seq[Int]): Seq[Int] = {
+
+      val maxFactor = n/2
+
+      (2 to maxFactor).find(n%_ == 0) match {
+        case Some(factor) => innerPrimeFactor(n/factor, factor +: factors)
+        case None => n +: factors
+      }
+    }
+
+    innerPrimeFactor(n, Seq.empty[Int])
+  }
 }
--- src/test/scala/primefactors/PrimeFactorsTest.scala
+++ src/test/scala/primefactors/PrimeFactorsTest.scala
@@ -5,7 +5,7 @@ import org.scalatest.{Matchers, WordSpec}
 class PrimeFactorsTest extends WordSpec with Matchers {
 
   "PrimeFactors" should {
-    "return" when {
+    "return all factors" when {
       "called with 2" in {
         PrimeFactors.primeFactors(2) shouldBe Seq(2)
       }
@@ -16,6 +16,26 @@ class PrimeFactorsTest extends WordSpec with Matchers {
 
       "called with 4" in {
         PrimeFactors.primeFactors(4) shouldBe Seq(2, 2)
+      }
+
+      "called with 5" in {
+        PrimeFactors.primeFactors(5) shouldBe Seq(5)
+      }
+
+      "called with 6" in {
+        PrimeFactors.primeFactors(6) should contain allElementsOf Seq(2, 3)
+      }
+
+      "called with 7" in {
+        PrimeFactors.primeFactors(7) shouldBe Seq(7)
+      }
+
+      "called with 8" in {
+        PrimeFactors.primeFactors(8) shouldBe Seq(2, 2, 2)
+      }
+
+      "called with 9" in {
+        PrimeFactors.primeFactors(9) shouldBe Seq(3, 3)
       }
     }
   }
